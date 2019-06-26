a7f190282088cd4d0283389893a80a1842333784
Irene Mutua <mutuamirene.gmail.com>
2019-06-26 12:21:40 +0300

    Created App and Form components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "./Form";
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Form />
+            </div>
+        )
+    }
+}
---
+++ src/Form.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class Form extends React.Component {
+    render() {
+        return (
+            <div>
+                <form>
+                <label>Label</label>
+                <input type="text" placeholder="This is a palceholder" />
+                </form>
+              
+            </div>
+        )
+    }
+}
