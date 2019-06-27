464a954cc4cdf6ce10f730464ac2b137ebe1aca8
Irene Mutua <mutuamirene.gmail.com>
2019-06-27 11:03:33 +0300

    Updated the components

--- src/App.js
+++ src/App.js
@@ -3,18 +3,62 @@ import Form from "./Form";
 import "./App.css"
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+        this.getBmi = this.getBmi.bind(this);
+    }
+    /*computeBmi() {
+        let bmiValue = (this.state.weight / this.state.height);
+        this.setState({
+            bmi: bmiValue
+        })
+        let bmiClass = this.getBmi(bmiValue);
+        this.setState({ bmiClass: bmiClass });
+    }*/
+    getBmi(bmi) {
+        if (bmi < 18.5) {
+            return "Underweight";
+        }
+        if (bmi >= 18.5 && bmi < 24.9) {
+            return "Normal weight";
+        }
+        if (bmi >= 25 && bmi < 29.9) {
+            return "Overweight";
+        }
+        if (bmi >= 30) {
+            return "Obesity";
+        }
+    }
+    
     render() {
         return (
             <div className="inputs">
                 <div className="row">
-                    <Form label="Height" placeholder="Enter height in metres" /> 
+                <p style={{textAlign:"center", marginLeft: "20px", fontWeight: "bold"}}>BMI CALCULATOR</p>
+                    <Form label="Weight" placeholder="Enter weight in kgs" onChange={this.weightChange} />
                     <br />
                 </div>
+                <div className="row">
+                    <Form label="Height" placeholder="Enter height in metres" onChange={this.heightChange} />
+                </div>
                 <div>
-                    <Form label="Weight" placeholder="Enter weight in kgs"/> 
-              </div>
-                
-                
+                    <button
+                        style={{ padding: "10px", margin: "10px 0px 0px 120px" }}
+                        onClick={this.handleClick}>
+                        Submit
+                        </button>
+                </div>
+               
+                 <div className="row">
+                    <h3>BMI = {this.state.bmi}</h3>
+                </div>
+                <div className="row">
+                    <h3>{this.state.bmiClass}</h3>
+                </div>
+
             </div>
 
         )
--- src/Form.js
+++ src/Form.js
@@ -1,14 +1,67 @@
 import React from "react";
 
 export default class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: "",
+            weight: "",
+            height: "",
+            bmi: ""
+            
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.heightChange = this.heightChange.bind(this);
+        this.weightChange = this.weightChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+   
+    }
+    heightChange(heightValue) {
+        this.setState({
+            height: heightValue
+        })
+    }
+    weightChange(weightValue) {
+        this.setState({
+            weight: weightValue
+        })
+    }
+    handleClick() {
+        let bmiValue = (this.state.weight / this.state.height);
+        this.setState({
+            bmi: bmiValue
+        })
+        let bmiClass = this.getBmi(bmiValue);
+        this.setState({ bmiClass: bmiClass });
+    }
+   
+    handleChange(event) {
+        let inputValue = event.target.value;
+        this.setState({
+            value: inputValue
+        });
+
+    }
     render() {
         return (
-            <div className="row">
-                <label>{this.props.label}</label>
-                <input style={{padding:"20px", marginLeft:"10px"}}type="text" placeholder={this.props.placeholder} />
-                
+            <div>
+                   
+                <div className="row">
               
+                    <form>
+                <label>{this.props.label}</label>
+                <input
+                    style={{ padding: "20px", marginLeft: "10px" }}
+                    type="text"
+                    placeholder={this.props.placeholder}
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                        />
+                        </form>
             </div>
+            
+            </div>
+            
         )
     }
 }
